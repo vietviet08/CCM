b20b8973de5c846dc151948b1ad9a20d48a7c44c
vietviet08 <vietnq.23ceb@vku.udn.vn>
2025-02-28 00:07:38 +0700

    the merge mv to mastyer

--- src/main/java/com/ccm/App.java
+++ src/main/java/com/ccm/App.java
@@ -2,10 +2,8 @@ package com.ccm;
 
 import com.ccm.view.LoginForm;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         LoginForm.main(args);
     }
 }
